f825e3da1da1ad1abc29a137b643896bbbaccaa2
Vegard Schøyen Houg <vegard.houg@revolve.no>
2025-02-19 13:32:34 +0100

    Added function delcarations in elevator control

--- src/elevator_control.h
+++ src/elevator_control.h
@@ -1,3 +1,19 @@
+#pragma once
+
 #include "driver/elevio.h"
 
+typedef enum {
+    IDLE,
+    MOVING,
+    DOOR_OPEN,
+    STOPPED
+} ElevatorState;
 
+static ElevatorState current_state;
+static int current_floor;
+
+void elevator_init();
+
+void elevator_update();
+
+voie elevator_move();
