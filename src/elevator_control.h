02b7940fe98003a1b85cda878807d5e1c48da68a
Vegard Schøyen Houg <vegard.houg@revolve.no>
2025-03-05 22:54:53 +0100

    Added Doxygen style function documentation

--- src/elevator_control.h
+++ src/elevator_control.h
@@ -24,14 +24,37 @@ typedef struct {
 
 /**
  * @brief Initialize the elevator.
+ * @details The elevator is initialized to a known state, with exception handling for the case where the elevator is in an unknown state.
+ * @return void
+ */
 void elevator_init();
 
-void elevator_update();
-
+/**
+ * @brief Move the elevator.
+ * @return void
+ */
 void elevator_move();
 
+/**
+ * @brief Update the elevator.
+ * @details Update the elevator state based on the next target floor.
+ * @param[in] next_target_floor 
+ * @return void
+ */
+void elevator_update(int next_target_floor);
+
+/**
+ * @brief Handle an emergency stop.
+ * @details Stop the elevator reset state and target.
+ * @return void
+ */
 void handle_emergency_stop();
 
+/**
+ * @brief Handle an obstruction.
+ * @details Stop the elevator and open the door.
+ * @return void
+ */
 void handle_obstruction();
 
 #endif // ELEVATOR_CONTROL_H
